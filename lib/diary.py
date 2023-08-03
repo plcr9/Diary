f09ed51f7a9f8598b5bed21a9d841886f5c203e5
plcr9 <paulcooperpc9@gmail.com>
2023-08-03 17:42:10 +0100

    Reading Time method and test added

--- lib/diary.py
+++ lib/diary.py
@@ -1,3 +1,5 @@
+import math
+
 class Diary():
     def __init__(self):
         self._entries = []
@@ -12,5 +14,6 @@ class Diary():
         word_counts = ([entry.count_words() for entry in self._entries])
         return sum(word_counts)
 
-    def reading_time(self):
-        pass
+    def reading_time(self, wpm):
+        word_count = self.count_words()
+        return math.ceil(word_count/wpm)
--- tests/test_integration.py
+++ tests/test_integration.py
@@ -15,3 +15,13 @@ def test_count_words_returns_count_of_all_words_in_all_entry_contents():
     diary.add(entry_1)
     diary.add(entry_2)
     assert diary.count_words() == 5
+
+'''If two diary entries added with a total length of 5 and a reading time of 2wpm, it should return total reading time of 3'''
+def test_reading_time_returns_time_to_read_all_entries():
+    diary = Diary()
+    entry_1 = DiaryEntry("My Title 1", "One Two")
+    entry_2 = DiaryEntry("My Title 2", "Three Four Five")
+    diary.add(entry_1)
+    diary.add(entry_2)
+    assert diary.reading_time(2) == 3
+    
