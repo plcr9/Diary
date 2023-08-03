b7fee140647f3600536e8300dd58f731bcdaa8dc
plcr9 <paulcooperpc9@gmail.com>
2023-08-03 17:37:06 +0100

    Zero word count test added

--- tests/test_diary.py
+++ tests/test_diary.py
@@ -4,3 +4,8 @@ from lib.diary import Diary
 def test_initially_has_empty_list_of_entries():
     diary = Diary()
     assert diary.all() == []
+
+'''Initially, word count is zero'''
+def test_initially_word_count_is_zero():
+    diary = Diary()
+    assert diary.count_words() == 0
